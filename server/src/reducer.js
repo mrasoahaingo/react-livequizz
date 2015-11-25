f937a0b5bdd6be366dd40ffcc25b911eb0bbcf1d
mrasoahaingo <michael.rasoahaingo@gmail.com>
2015-11-25 14:19:03 +0100

    Handle reducers

---
+++ server/src/reducer.js
@@ -0,0 +1,26 @@
+import { setEntries, setPlayers, next, buzz, rightResponse, wrongResponse } from './core'
+
+export default function reducer(state, action) {
+    switch (action.type) {
+        case 'SET_ENTRIES':
+        return setEntries(state, action.entries);
+
+        case 'SET_PLAYERS':
+        return setPlayers(state, action.players);
+
+        case 'NEXT':
+        return next(state);
+
+        case 'BUZZ':
+        return buzz(state, action.playerId);
+
+        case 'RIGHT_RESPONSE':
+        return rightResponse(state);
+
+        case 'WRONG_RESPONSE':
+        return wrongResponse(state);
+
+        default:
+        return state;
+    }
+}
---
+++ server/test/reducer_spec.js
@@ -0,0 +1,110 @@
+import {Map, fromJS} from 'immutable'
+import { expect } from 'chai'
+
+import reducer from '../src/reducer'
+
+describe('reducer', () => {
+    it('handle set entries', () => {
+        const initialState = Map();
+        const entries = [
+            { question: 'Where is Bryan', response: 'In the kitchen' },
+            { question: 'What is Sandy lastname', response: 'Kilo' }
+        ];
+        const action = { type: 'SET_ENTRIES', entries };
+        const nextState = reducer(initialState, action);
+        expect(nextState).to.equal(fromJS({
+            entries: [
+                { question: 'Where is Bryan', response: 'In the kitchen' },
+                { question: 'What is Sandy lastname', response: 'Kilo' }
+            ]
+        }))
+    });
+
+    it('handle set players', () => {
+        const initialState = Map();
+        const players = [ 'Bryan', 'Sandy' ];
+        const action = { type: 'SET_PLAYERS', players };
+        const nextState = reducer(initialState, action);
+        expect(nextState).to.equal(fromJS({
+            players: [ 'Bryan', 'Sandy' ]
+        }))
+    });
+
+    it('handle next step', () => {
+        const initialState = fromJS({
+            entries: [
+                { question: 'Where is Bryan', response: 'In the kitchen' },
+                { question: 'What is Sandy lastname', response: 'Kilo' }
+            ]
+        });
+        const action = { type: 'NEXT' };
+        const nextState = reducer(initialState, action);
+        expect(nextState).to.equal(fromJS({
+            quizz: { question: 'Where is Bryan', response: 'In the kitchen' },
+            entries: [
+                { question: 'What is Sandy lastname', response: 'Kilo' }
+            ]
+        }))
+    });
+
+    it('handle buzzer', () => {
+        const initialState = fromJS({
+            quizz: { question: 'Where is Bryan', response: 'In the kitchen' },
+            entries: [
+                { question: 'What is Sandy lastname', response: 'Kilo' }
+            ]
+        });
+        const action = { type: 'BUZZ', playerId: 'Bryan' };
+        const nextState = reducer(initialState, action);
+        expect(nextState).to.equal(fromJS({
+            buzzer: 'Bryan',
+            quizz: { question: 'Where is Bryan', response: 'In the kitchen' },
+            entries: [
+                { question: 'What is Sandy lastname', response: 'Kilo' }
+            ]
+        }))
+    });
+
+    it('handle right response', () => {
+        const initialState = fromJS({
+            buzzer: 'Bryan',
+            quizz: { question: 'Where is Bryan', response: 'In the kitchen' },
+            entries: [
+                { question: 'What is Sandy lastname', response: 'Kilo' }
+            ]
+        });
+        const action = { type: 'RIGHT_RESPONSE' };
+        const nextState = reducer(initialState, action);
+        expect(nextState).to.equal(fromJS({
+            scores: {
+                'Bryan': 1
+            },
+            entries: [
+                { question: 'What is Sandy lastname', response: 'Kilo' }
+            ],
+            archive: [
+                { question: 'Where is Bryan', response: 'In the kitchen', buzzer: 'Bryan' }
+            ]
+        }))
+    });
+
+    it('handle wrong response', () => {
+        const initialState = fromJS({
+            buzzer: 'Bryan',
+            quizz: { question: 'Where is Bryan', response: 'In the kitchen' },
+            entries: [
+                { question: 'What is Sandy lastname', response: 'Kilo' }
+            ]
+        });
+        const action = { type: 'WRONG_RESPONSE' };
+        const nextState = reducer(initialState, action);
+        expect(nextState).to.equal(fromJS({
+            out: [ 'Bryan' ],
+            quizz: { question: 'Where is Bryan', response: 'In the kitchen' },
+            entries: [
+                { question: 'What is Sandy lastname', response: 'Kilo' }
+            ]
+        }))
+    });
+
+});
